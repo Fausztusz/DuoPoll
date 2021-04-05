eba109576edfbdb7f03eb590e1f1d50d8d9481fc
Fauszt Andras <fauszt.andras19@gmail.comi>
2021-04-05 22:13:47 +0200

    Add MVC

---
+++ IHateThisSh_t.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IHateThisSh_t.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IHateThisSh_t.MVC/Routes/Web.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using SQLitePCL;
+
+namespace IHateThisSh_t.MVC.Routes
+{
+    public static class Web
+    {
+        static Web()
+        {
+        }
+
+        public static IApplicationBuilder Route(IApplicationBuilder app)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "users",
+                    pattern: "{controller=Users}/{action=Users}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+            return app;
+        }
+    }
+}

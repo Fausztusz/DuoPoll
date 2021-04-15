d50a888c8c3eaa08eb15e834055bed1cd3d02aee
Fauszt Andras <fauszt.andras19@gmail.comi>
2021-04-15 03:13:40 +0200

    Add fonts

--- DuoPoll.MVC/tailwind.config.js
+++ DuoPoll.MVC/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     purge: [
         './Resources/**/*.css',
@@ -6,6 +8,19 @@ module.exports = {
     ],
     darkMode: 'media', // false or 'media' or 'class'
     theme: {
+        fontFamily: {
+            'display': ['Copse', 'Oswald', ...defaultTheme.fontFamily.sans],
+            'body': ['Roboto Slab', 'Roboto', 'Open Sans', ...defaultTheme.fontFamily.serif],
+            'sans': ['Open Sans', ...defaultTheme.fontFamily.sans],
+        },
+        borderRadius: {
+            'none': '0',
+            'sm': '0.125rem',
+            'md': '0.375rem',
+            'lg': '0.5rem',
+            'full': '9999px',
+            'large': '20px',
+        },
         extend: {},
     },
     variants: {
